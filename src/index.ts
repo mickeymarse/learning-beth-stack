b737546594c3e9fac74e554af84445c4c5f7301d
mickeymarse <mmserrapica@tutanota.com>
2024-03-06 12:26:49 +0000

    Added 404 message to paths.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import figlet from 'figlet';
 import { Elysia } from 'elysia';
 
-const app = new Elysia().get('/', () => figlet.textSync('Hello Elysia')).listen(8080);
+const app = new Elysia()
+  .get('/', () => figlet.textSync('Hello Elysia'))
+  .onError(({ code }) => {
+    if (code === 'NOT_FOUND') return figlet.textSync(`Route not found :'(`);
+  })
+  .listen(8080);
 
 console.log(`🦊 Elysia is running at http://localhost:${app.server?.port}...`);
