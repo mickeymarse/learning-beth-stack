4be500ab96db444e51c035b7ad5ae4e8a874a93f
mickeymarse <mmserrapica@tutanota.com>
2024-03-07 17:33:26 +0000

    Starting Turso and Drizzle setup

---
+++ src/db/schema.ts
@@ -0,0 +1,14 @@
+import { createInsertSchema, createSelectSchema } from 'drizzle-typebox';
+import { text, integer, sqliteTable } from 'drizzle-orm/sqlite-core';
+
+export const todos = sqliteTable('todos', {
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+  content: text('content').notNull(),
+  completed: integer('completed', { mode: 'boolean' }).notNull().default(false),
+});
+
+export type Todo = typeof todos.$inferSelect;
+export type InsertTodo = typeof todos.$inferInsert;
+
+export const insertTodoSchema = createInsertSchema(todos);
+export const selectTodoSchema = createSelectSchema(todos);
